5407dcbe4d62e35cb8ad5ad6e6032cb0d8ae6814
Junior <juniorfajardom@gmail.com>
2024-08-23 20:09:43 -0500

    Creacion de views y urls

--- crear/views.py
+++ crear/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Tarea
+from .forms import TareaForm
 # Create your views here.
+
+def creacion(request):
+    if request.method == 'POST':
+        form = TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
+    form = TareaForm()
+    return render(request,'creacion.html',{'form':form})
+
+def listar(request):
+    tareas = Tarea.objects.all()
+    return render(request,'listar.html',{'tareas':tareas})
