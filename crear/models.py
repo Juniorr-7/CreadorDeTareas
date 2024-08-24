5537210da24c430b6b90a59aa52fc3996a700c97
Junior <juniorfajardom@gmail.com>
2024-08-23 20:06:05 -0500

    Modelo y formulario del modelo creados

--- crear/models.py
+++ crear/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    ESTADO_CHOICES = [('finalizada','Finalizada'),('incompleta','Incompleta')]
+    titulo = models.CharField(max_length=20)
+    descripcion = models.TextField(max_length=200)
+    estado = models.CharField(max_length=50,choices=ESTADO_CHOICES,default='incompleta')
+    def __str__(self):
+        return self.titulo
